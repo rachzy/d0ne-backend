6d2bbb3e07fc34aa0f322585057d37371782433d
Henrique <henriqueferreira.fox@gmail.com>
2023-11-23 23:17:52 -0300

    create auth guard

---
+++ src/users/guards/auth.guard.ts
@@ -0,0 +1,44 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Request, Response } from 'express';
+import { UsersService } from '../users.service';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(private readonly usersService: UsersService) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const ctx = context.switchToHttp();
+    const response = ctx.getResponse() as Response;
+    const request = ctx.getRequest() as Request;
+
+    const { UID, STOKEN } = request.cookies;
+
+    function errorCallback(message: string) {
+      response.clearCookie('UID');
+      response.clearCookie('STOKEN');
+      throw new UnauthorizedException(message);
+    }
+
+    if (!UID || !STOKEN) {
+      errorCallback('User is not authenticated!');
+      return false;
+    }
+
+    const securityTokenStatus = this.usersService.checkSecurityTokenStatus(
+      UID,
+      STOKEN,
+    );
+
+    if (!securityTokenStatus) {
+      errorCallback('Invalid security token!');
+      return false;
+    }
+
+    return true;
+  }
+}
